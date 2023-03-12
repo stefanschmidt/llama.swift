7e27bbc02ed488b3800283239dfd6366317199a6
Alex Rozanski <alex@rozanski.me>
2023-03-12 09:32:31 +0100

    add basic Xcode project and include cpp files

---
+++ llama/llama.h
@@ -0,0 +1,18 @@
+//
+//  llama.h
+//  llama
+//
+//  Created by Alex Rozanski on 12/03/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for llama.
+FOUNDATION_EXPORT double llamaVersionNumber;
+
+//! Project version string for llama.
+FOUNDATION_EXPORT const unsigned char llamaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <llama/PublicHeader.h>
+
+
