5f4905a25674cb875e38179320756c6a9bed3b8e
Alex Rozanski <alex@rozanski.me>
2023-03-16 00:24:12 +0100

    separate Swift and C++/Objective-C++ code into separate libraries

---
+++ Sources/llama/llama-Bridging-Header.h
@@ -0,0 +1,10 @@
+//
+//  Llama-Bridging-Header.h
+//  llama
+//
+//  Created by Alex Rozanski on 15/03/2023.
+//
+
+#import <llamaObjCxx/LlamaRunnerBridge.h>
+#import <llamaObjCxx/LlamaRunnerBridgeConfig.h>
+#import <llamaObjCxx/LlamaEvent.h>
